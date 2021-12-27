594f78ba24e8490bc423739af7381cafafbc324c
Juan <juandavidlivingston@gmail.com>
2021-12-27 12:33:26 -0300

    Add button component

---
+++ src/components/ui/button/Button.stories.js
@@ -0,0 +1,69 @@
+import Button from './Button.vue'
+import { Icon } from '@vicons/utils'
+import { Accessible } from '@vicons/tabler'
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: ['blue', 'gray', 'green', 'red'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['sm', 'md', 'lg'],
+      },
+    },
+  },
+}
+
+const TemplateIcon = (args) => ({
+  components: { Button, Icon, Accessible },
+  setup() {
+    return { args }
+  },
+  template: `<Button v-bind="args"> 
+    <Icon>
+      <Accessible/>
+    </Icon>
+  </Button>`,
+})
+
+const TemplateText = (args) => ({
+  components: { Button, Icon, Accessible },
+  setup() {
+    return { args }
+  },
+  template: `<Button v-bind="args"> 
+    Hello word!
+  </Button>`,
+})
+
+const TemplateIconText = (args) => ({
+  components: { Button, Icon, Accessible },
+  setup() {
+    return { args }
+  },
+  template: `<Button v-bind="args"> 
+    <Icon>
+      <Accessible/>
+    </Icon>
+    
+    <span class="pl-1">
+      Hello word!
+    </span>
+  </Button>`,
+})
+
+export const OnlyIcon = TemplateIcon.bind({})
+OnlyIcon.args = { color: 'gray', size: 'md' }
+
+export const OnlyText = TemplateText.bind({})
+OnlyText.args = { color: 'gray', size: 'md' }
+
+export const IconText = TemplateIconText.bind({})
+IconText.args = { color: 'gray', size: 'md' }
---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,64 @@
+<template>
+  <button
+    class="button"
+    :class="{ [`button-${color}`]: true, [`button-${size}`]: true }"
+    :type="type"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+  import { PropType } from 'vue'
+
+  type Color = 'blue' | 'gray' | 'red' | 'green'
+  type Size = 'sm' | 'md' | 'lg'
+  type Type = 'button' | 'submit' | 'reset'
+
+  defineProps({
+    color: { type: String as PropType<Color>, default: 'gray' },
+    size: { type: String as PropType<Size>, default: 'md' },
+    type: { type: String as PropType<Type>, default: 'button' },
+  })
+</script>
+
+<style scoped>
+  .button {
+    border: none;
+    cursor: pointer;
+    border-radius: 0.25rem;
+    display: flex;
+    align-items: center;
+  }
+  .button-sm {
+    font-size: 0.7rem;
+    height: 1.25rem;
+  }
+  .button-md {
+    font-size: 0.85rem;
+    height: 1.5rem;
+  }
+  .button-lg {
+    font-size: 1rem;
+    height: 1.75rem;
+  }
+  .button-blue {
+    color: white;
+    background-color: var(--blue);
+  }
+  .button-gray {
+    color: black;
+    background-color: var(--gray);
+  }
+  .button-red {
+    color: white;
+    background-color: var(--red);
+  }
+  .button-green {
+    color: white;
+    background-color: var(--green);
+  }
+  .button:hover {
+    opacity: 0.8;
+  }
+</style>
