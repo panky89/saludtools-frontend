532d08fbe3a4b431221b2530be8ef3e4181d29a9
Juan <juandavidlivingston@gmail.com>
2021-12-28 10:23:51 -0300

    Add messages error

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -1,58 +1,87 @@
 <template>
-  <div
-    class="input-wrapper"
-    :style="{ height: type === 'area' ? '100%' : '40px' }"
-  >
-    <label class="input-label" :for="name" :class="{ block: value }">
-      {{ label || placeholder }}
-    </label>
+  <div class="field">
+    <div
+      class="input-wrapper"
+      :style="{ height: type === 'area' ? '100%' : '40px' }"
+    >
+      <label
+        class="input-label"
+        :for="name"
+        :class="{ 'input-label__show': value }"
+      >
+        {{ label || placeholder }}
+      </label>
 
-    <input
-      v-if="type !== 'area'"
-      class="input"
-      :name="name"
-      :id="id"
-      :placeholder="placeholder"
-      :value="value"
-      :type="type"
-      @input="onInput"
-      @keyup.enter="$emit('keyup.enter')"
-    />
+      <input
+        v-if="type !== 'area'"
+        class="input"
+        :name="name"
+        :id="id"
+        :placeholder="placeholder"
+        :value="value"
+        :type="type"
+        @input="onInput"
+        @blur="onBlur"
+        @keyup.enter="$emit('keyup.enter')"
+      />
 
-    <textarea
-      v-else
-      class="input textarea"
-      :name="name"
-      :id="id"
-      :placeholder="placeholder"
-      :value="value"
-      cols="4"
-      @input="onInput"
-      @keyup.enter="$emit('keyup.enter')"
-    />
+      <textarea
+        v-else
+        class="input textarea"
+        :name="name"
+        :id="id"
+        :placeholder="placeholder"
+        :value="value"
+        cols="4"
+        @input="onInput"
+        @blur="onBlur"
+        @keyup.enter="$emit('keyup.enter')"
+      />
+    </div>
+
+    <span v-if="touched && error" class="field__error">
+      {{ error }}
+    </span>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
+  import { ref, watchEffect } from 'vue'
 
-  defineProps({
+  const props = defineProps({
     id: { type: String, default: '' },
     name: { type: String, default: '' },
     placeholder: { type: String, default: '' },
     modelValue: { type: String, default: '' },
     label: { type: String, default: '' },
     type: { type: String, default: 'text' },
+    error: { type: String, default: '' },
   })
 
+  const touched = ref(false)
   const value = ref('')
-  const emit = defineEmits(['update:modelValue', 'keyup.enter'])
+  const emit = defineEmits([
+    'update:modelValue',
+    'keyup.enter',
+    'input',
+    'blur',
+  ])
 
   function onInput(e: Event) {
     const target = e.target as HTMLInputElement
     value.value = target.value
     emit('update:modelValue', value.value)
+    emit('input', value.value)
   }
+
+  function onBlur(e: Event) {
+    emit('blur', e)
+    touched.value = true
+  }
+
+  watchEffect(() => {
+    value.value = props.modelValue
+  })
 </script>
 
 <style scoped>
@@ -61,6 +90,8 @@
     background-color: rgb(240, 240, 240);
     border-radius: 4px;
     border: 2px solid transparent;
+    border-top-width: 6px;
+    border-bottom-width: px;
     width: max-content;
     transition: opacity 150ms cubic-bezier(0.4, 0, 0.2, 1);
     width: 100%;
@@ -103,5 +134,13 @@
   .input-wrapper:focus-within .input::placeholder {
     color: transparent;
     font-size: 0.9rem;
+  }
+  .input-label__show {
+    display: block;
+  }
+  .field__error {
+    text-transform: capitalize;
+    color: var(--red);
+    font-size: 0.75rem;
   }
 </style>
